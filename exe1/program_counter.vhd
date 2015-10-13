98d036e6f31eef2f19ab0ca51835950344755b84
Jocelyn Pierre Gounon <jocelyng@dmlab03.(none)>
2015-10-13 12:21:29 +0200

    program counter added

---
+++ exe1/program_counter.vhd
@@ -0,0 +1,36 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+
+entity program_counter is
+    port( 
+
+	clk		: in std_logic; -- clock
+	rst		: in std_logic;
+        write_en 	: in std_logic;
+	
+	PC_in		: in std_logic_vector(31 downto 0); -- address input
+	PC_out		: out std_logic_vector(31 downto 0) ); -- address output
+
+end entity program_counter;
+
+
+architecture behavioural of program_counter is
+
+
+begin
+	
+    process (clk, rst, write_en) is
+    begin
+		 if clk'event and clk='1' then
+			if rst='1' then
+			  PC_out <= (others => '0');
+			elsif rst='0' and write_en='1' then
+			  PC_out <= PC_in;
+			end if;
+		 end if;
+	 end process;
+
+end architecture behavioural;
