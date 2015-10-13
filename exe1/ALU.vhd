bb446089255ab632e415e8b87df26e6d7580ef7e
Sondre Basma <scbasma@github.com>
2015-10-13 10:57:03 +0000

    Added ALU and ALU_Control

---
+++ exe1/ALU.vhd
@@ -0,0 +1,36 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity ALU is
+
+  port (
+    rt: in std_logic_vector (31 downto 0);
+    rs: in std_logic_vector (31 downto 0);
+
+    alu_op: in std_logic_vector(3 downto 0);
+
+    alu_result: out std_logic_vector(31 downto 0);
+    zero: out std_logic 
+
+  );
+
+end ALU;
+
+architecture behavorial of ALU is
+begin
+
+  zero <= (alu_result = 0);
+
+  process (alu_op, rt, rs)
+  begin
+    case alu_op is
+      when "0010" => result <= signed(rt) + signed(rs);
+      when "0110" => result <= signed(rt) - signed(rs);
+      when "0111" => result <= if rt < rs then result <= '1' else result<= '0' end if;
+      when "0000" => result <= rt and rs;
+      when "0001" => result <= rt or rs;
+    end case;
+  end process;
+end behavorial;
+
---
+++ exe1/ALU_control.vhd
@@ -0,0 +1,36 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMBERIC_STD.ALL;
+
+entity ALU_Ctrl is 
+
+  port (
+      
+      op_code: in std_logic_vector(1 downto 0);
+      instruction_funct: in std_logic_vector(5 downto 0);
+      
+      alu_op: out std_logic_vector(5 downto 0)
+
+  );
+
+end ALU_Ctrl;
+
+architecture behavorial of ALU_Ctrl is 
+begin 
+
+  process(op_code, instruction_funct)
+  begin
+    case op_code is 
+      when "00" => alu_op <= "0010";
+      when "01" => alu_op <= "0110";
+      when "10" => 
+          case instruction_funct is
+            when "100000" => alu_op <= "0010";
+            when "100010" => alu_op <= "0110";
+            when "100100" => alu_op <= "0000";
+            when "100101" => alu_op <= "0001";
+            when "101010" => alu_op <= "0111";      
+          end case;
+    end case;
+  end process;
+end behavorial;
