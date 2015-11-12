15dca0953b8ffe368479be5020611892388cd27c
Espen Strømjordet <espstr@stud.ntnu.no>
2015-11-12 20:41:00 +0100

    Program counter TB succeeds

--- exe2/testbench/program_counter_tb.vhd
+++ exe2/testbench/program_counter_tb.vhd
@@ -18,7 +18,9 @@ architecture behavioural of program_counter_tb is
   signal rst      : std_logic := '0';
   signal PC_in	  : std_logic_vector(31 downto 0) := (others => '0');
   signal write_en : std_logic := '0';
-
+  signal flush_in : std_logic := '0';
+  signal flush_out : std_logic := '0';
+  signal stall : std_logic := '0';
   signal PC_out   : std_logic_vector(31 downto 0) := (others => '0');
 
   -- clock
@@ -34,6 +36,9 @@ begin  -- architecture behavioural
       rst      => rst,
       PC_in    => PC_in,
       write_en => write_en,
+	  flush_in => flush_in,
+	  flush_out => flush_out,
+	  stall => stall,
       PC_out   => PC_out);
 
   -- clock generation
@@ -43,6 +48,7 @@ begin  -- architecture behavioural
   WaveGen_Proc: process
   begin
     -- insert signal assignments here
+	stall <= '1';
     wait for clk_period/4;
     rst <= '1';
     wait for clk_period;
@@ -57,6 +63,7 @@ begin  -- architecture behavioural
           "PC_out address should not be updated immediately after write_en");
     report "Test 2 passed" severity note;
 	 wait for clk_period;
+	 wait for clk_period;
     write_en <= '0';
     check(PC_out = x"00000004", "PC_out address should be reflected  the 2th addressafter write_en");
     report "Test 3 passed" severity note;
@@ -66,6 +73,7 @@ begin  -- architecture behavioural
 	 check(PC_out = x"00000004", "PC_out address should be reflected the 2th address");
     report "Test 4 passed" severity note;
 	 
+	 wait for clk_period;
 	 wait for clk_period;
     write_en <= '0';
     check(PC_out = x"00000008", "PC_out address should be reflected  the 3rd addressafter write_en");
@@ -80,6 +88,7 @@ begin  -- architecture behavioural
 	 check(PC_out = x"00000008", "PC_out address should be reflected the 3rd address");
     report "Test 7 passed" severity note;
 	 
+	 wait for clk_period;
 	 wait for clk_period;
     write_en <= '0';
     check(PC_out = x"000000E0", "PC_out address should be reflected  the 4th addressafter write_en");
